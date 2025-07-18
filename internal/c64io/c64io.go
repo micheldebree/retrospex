1bc24a3ed23ce699741d08a6172bdc9203c383cd
Michel de Bree <michel@micheldebree.nl>
2025-07-18 16:52:16 +0200

    feat: add getBitmapData function to extract bitmap data

--- internal/c64io/c64io.go
+++ internal/c64io/c64io.go
@@ -10,5 +10,44 @@ import (
 // if the image's spec has a bitPatternSize of 1, 8 bit patterns are packed into one byte, msb to lsb order
 // if the image's spec has a bitPatternSize of 2, 4 bit patterns are packed into one byte, msb to lsb order
 // The order of the bytes in the files is the specific Commodore 64 layout of bytes in bitmap mode
-func getBitmapData(img *indexedimage.IndexedImage) {
+func getBitmapData(img *indexedimage.IndexedImage) []byte {
+    width, height := img.width, img.height
+    bitPatternSize := img.spec.bitsPatternSize
+
+    // Calculate the number of bytes needed
+    numBitPatterns := width * height
+    numBytes := (numBitPatterns + 7) / 8 // Ceiling division
+
+    result := make([]byte, numBytes)
+    byteIndex := 0
+    bitPosition := 0
+
+    for y := 0; y < height; y++ {
+        for x := 0; x < width; x++ {
+            pixelIndex := y*width + x
+            pixel := &img.pixels[pixelIndex]
+            pixel.assertHasBitPattern() // Ensure the pixel has a bit pattern
+
+            bitPattern := pixel.BitPattern
+            if bitPatternSize == 1 {
+                result[byteIndex] |= byte(bitPattern) << (7 - bitPosition)
+                bitPosition++
+                if bitPosition == 8 {
+                    bitPosition = 0
+                    byteIndex++
+                }
+            } else if bitPatternSize == 2 {
+                result[byteIndex] |= byte(bitPattern) << (6 - bitPosition)
+                bitPosition += 2
+                if bitPosition == 8 {
+                    bitPosition = 0
+                    byteIndex++
+                }
+            } else {
+                panic("Unsupported bit pattern size")
+            }
+        }
+    }
+
+    return result
 }
