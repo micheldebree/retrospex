84735d8545d1cf30e9510840dac61fef65193ca9
Michel de Bree <michel@micheldebree.nl>
2025-07-18 16:27:14 +0200

    feat: add function to extract C64 bitmap data

--- internal/c64io/c64io.go
+++ internal/c64io/c64io.go
@@ -4,5 +4,11 @@ import (
 	"github.com/micheldebree/retrospex/internal/indexedimage"
 )
 
+// This function returns an array of bytes representing the bitpatterns assigned to the pixels in the image.
+// If a pixel has no bitpattern assigned, it panics
+// Bitpatterns are packed into bytes;
+// if the image's spec has a bitPatternSize of 1, 8 bit patterns are packed into one byte, msb to lsb order
+// if the image's spec has a bitPatternSize of 2, 4 bit patterns are packed into one byte, msb to lsb order
+// The order of the bytes in the files is the specific Commodore 64 layout of bytes in bitmap mode
 func getBitmapData(img *indexedimage.IndexedImage) {
 }
